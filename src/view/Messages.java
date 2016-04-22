37fbdd353ef39ceadaa3879cefbcfe96f05df182
Josh Riccio <jriccio@email.arizona.edu>
2016-04-22 15:39:59 -0700

    added scrollpanes to chat and message windows

--- src/view/Messages.java
+++ src/view/Messages.java
@@ -5,19 +5,23 @@ import java.awt.Color;
 import java.awt.Dimension;
 
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.JTextPane;
 
 public class Messages extends JPanel{
 
 	private static final long serialVersionUID = 386111478676201121L;
 	JTextPane messages;
+	JScrollPane scrollpane;
 	
 	public Messages(){
 		this.messages = new JTextPane();
 		this.messages.setEditable(false);
 		this.messages.setBackground(Color.white);
 		this.messages.setPreferredSize(new Dimension(580, 300));
-		this.add(this.messages, BorderLayout.CENTER);
+		this.scrollpane = new JScrollPane(this.messages, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS,
+				JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+		this.add(this.scrollpane, BorderLayout.CENTER);
 		
 		this.messages.setText("What is your name? (Use ctrl+enter to send)");
 	}
--- src/view/TextArea.java
+++ src/view/TextArea.java
@@ -4,19 +4,23 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.JTextPane;
 
 public class TextArea extends JPanel{
 
 	private static final long serialVersionUID = -8716145497904982869L;
 	private JTextPane chat;
+	private JScrollPane scrollpane;
 	
 	public TextArea(JTextPane chat){
 		this.chat = chat;
 		this.chat.setEditable(true);
 		this.chat.setBackground(Color.white);
 		this.chat.setPreferredSize(new Dimension(580, 100));
-		this.add(this.chat, BorderLayout.CENTER);
+		this.scrollpane = new JScrollPane(this.chat, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS,
+				JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+		this.add(this.scrollpane, BorderLayout.CENTER);
 	}
 	
 	public JTextPane getText(){
