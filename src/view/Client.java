2e9052b2f14e82c58eaa2c02f1dd39dea01e2f7d
Josh Riccio <jriccio@email.arizona.edu>
2016-04-22 23:30:20 -0700

    Set the look and feel to Nimbus

--- src/view/Client.java
+++ src/view/Client.java
@@ -1,5 +1,8 @@
 package view;
 
+import javax.swing.UIManager;
+import javax.swing.UIManager.LookAndFeelInfo;
+
 /**
  * The main method for the client
  * 
@@ -9,6 +12,15 @@ package view;
 public class Client {
 
 	public static void main(String[] args) {
+		try {
+		    for (LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
+		        if ("Nimbus".equals(info.getName())) {
+		            UIManager.setLookAndFeel(info.getClassName());
+		            break;
+		        }
+		    }
+		} catch (Exception e) {
+		}
 		SideBar list = new SideBar();
 		list.setVisible(true);
 	}
